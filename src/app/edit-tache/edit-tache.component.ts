a9563c76bbab5ea2eea9a808064f3b11e0108030
saloum45 <saloumfall45@gmail.com>
2023-12-19 23:44:33 +0000

    in progress

--- src/app/edit-tache/edit-tache.component.ts
+++ src/app/edit-tache/edit-tache.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -12,41 +12,52 @@ import Swal from 'sweetalert2';
   templateUrl: './edit-tache.component.html',
   styleUrl: './edit-tache.component.css'
 })
-export class EditTacheComponent implements OnInit{
- // Attributs
- public titre = "";
- public contenu = "";
- public realise = "";
+export class EditTacheComponent implements OnInit {
+  // Attributs
+  public titre = "";
+  public contenu = "";
+  public realise = "";
 
- // Methodes
- constructor(private route:Router) {
-
- }
-  ngOnInit(): void {
+  // Methodes
+  constructor(private route: Router, private routeActivated: ActivatedRoute) {
 
   }
- // la fonction qui ajoute une tâche
- enregister() {
-   if (this.titre == "" || this.contenu == "") {
-     this.sweetMessage("Désolé!!!", "Veuillez renseigner tous les champs", "error");
-   } else {
-     let tachesTmp = JSON.parse(localStorage.getItem("lestaches") ?? '[]');
-     // incrementation de l'id pour la nouvelle tâche
-     let incremandedId=tachesTmp[tachesTmp.length-1].id+1;
-     tachesTmp.push({id:incremandedId, titre: this.titre, contenu: this.contenu, realise: this.realise });
-     localStorage.setItem("lestaches", JSON.stringify(tachesTmp));
-     this.route.navigate(['list']);
-     this.sweetMessage("Merci!!!", "Tâche ajoutée avec succès", "success");
-   }
+  ngOnInit(): void {
+    let tachesTmp = JSON.parse(localStorage.getItem("lestaches") ?? '[]');
+    tachesTmp.forEach((element: any) => {
+      if (element.id == this.routeActivated.snapshot.params['id']) {
+        this.titre = element.titre;
+        this.contenu = element.contenu;
+        this.realise = element.realise;
+      }
+    });
+  }
+  // la fonction qui ajoute une tâche
+  enregister() {
+    if (this.titre == "" || this.contenu == "") {
+      this.sweetMessage("Désolé!!!", "Veuillez renseigner tous les champs", "error");
+    } else {
+      let tachesTmp = JSON.parse(localStorage.getItem("lestaches") ?? '[]');
+      tachesTmp.forEach((element: any) => {
+        if (element.id == this.routeActivated.snapshot.params['id']) {
+          element.titre = this.titre;
+          element.contenu = this.contenu;
+          element.realise = this.realise;
+        }
+      });
+      localStorage.setItem("lestaches", JSON.stringify(tachesTmp));
+      this.route.navigate(['list']);
+      this.sweetMessage("Merci!!!", "Modifications faite avec succès", "success");
+    }
 
- }
+  }
 
- // la fonctionne qui affiche les popup d'alerte
- sweetMessage(title: any, text: any, icon: any) {
-   Swal.fire({
-     title: title,
-     text: text,
-     icon: icon
-   });
- }
+  // la fonctionne qui affiche les popup d'alerte
+  sweetMessage(title: any, text: any, icon: any) {
+    Swal.fire({
+      title: title,
+      text: text,
+      icon: icon
+    });
+  }
 }
