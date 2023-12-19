ebc0e8d063eacd9228c91fafc8e3f5223c40b6fb
saloum45 <saloumfall45@gmail.com>
2023-12-19 23:22:49 +0000

    in progress

--- src/app/list-tache/list-tache.component.ts
+++ src/app/list-tache/list-tache.component.ts
@@ -10,34 +10,34 @@ import Swal from 'sweetalert2';
 })
 export class ListTacheComponent implements OnInit {
   // Attributs
-  public lestaches:any[]=[];
+  public lestaches: any[] = [];
   public lesTachesParDefaut = [
     {
-      id:1,
+      id: 1,
       titre: "Concevoir un nouveau logo",
       contenu: "Exploration de concepts créatifs pour le logo de la marque.",
       realise: false
     },
     {
-      id:2,
+      id: 2,
       titre: "Optimiser la navigation mobile",
       contenu: "Adaptation de l'interface pour une expérience mobile conviviale.",
       realise: true
     },
     {
-      id:3,
+      id: 3,
       titre: "Créer une stratégie de marketing",
       contenu: "Élaboration d'une stratégie complète pour accroître la visibilité en ligne.",
       realise: false
     },
     {
-      id:4,
+      id: 4,
       titre: "Développer une fonction de recherche",
       contenu: "Intégration d'une fonction de recherche avancée pour améliorer l'expérience utilisateur.",
       realise: true
     },
     {
-      id:5,
+      id: 5,
       titre: "Tester les scénarios d'utilisation",
       contenu: "Exécution de scénarios de test pour identifier les points d'amélioration.",
       realise: true
@@ -47,15 +47,20 @@ export class ListTacheComponent implements OnInit {
 
   // Methodes
   ngOnInit(): void {
-    if (localStorage.getItem("lestaches")==null || undefined) {
-      localStorage.setItem("lestaches",JSON.stringify(this.lesTachesParDefaut));
-    }
-    this.lestaches=JSON.parse(localStorage.getItem("lestaches")??'[]');
-
+    // if (localStorage.getItem("lestaches")==null || undefined) {
+    //   localStorage.setItem("lestaches",JSON.stringify(this.lesTachesParDefaut));
+    // }
+    // this.lestaches=JSON.parse(localStorage.getItem("lestaches")??'[]');
+    this.initTaches();
   }
 
-
-  supression(){
+  initTaches() {
+    if (localStorage.getItem("lestaches") == null || undefined) {
+      localStorage.setItem("lestaches", JSON.stringify(this.lesTachesParDefaut));
+    }
+    this.lestaches = JSON.parse(localStorage.getItem("lestaches") ?? '[]');
+  }
+  supression(id: any) {
     Swal.fire({
       title: "Etes-vous sûr?",
       text: "La supreesion est irréversible",
@@ -67,6 +72,19 @@ export class ListTacheComponent implements OnInit {
       cancelButtonText: "Non"
     }).then((result) => {
       if (result.isConfirmed) {
+        // alert(id)
+        let tachesTmp = JSON.parse(localStorage.getItem("lestaches") ?? '[]');
+        // ici je recherche l'index de l'element que je veux supprimer
+        tachesTmp.forEach((element: any) => {
+          if (element.id == id) {
+            // une fois que je trouve l'index de l'element à supprimer je le passe à methode splice qui se charge de le supprimer
+            tachesTmp.splice(tachesTmp.indexOf(element), 1);
+          }
+
+        });
+        localStorage.setItem("lestaches", JSON.stringify(tachesTmp));
+        this.lestaches = JSON.parse(localStorage.getItem("lestaches") ?? '[]');
+
         Swal.fire({
           title: "Supprimé!",
           text: "Suppression faite avec succès",
